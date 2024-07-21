7e8782b25a43c0289bcfbdf781f3e0f771d57380
Андрей Урамаев <aruramaev@gmail.com>
2024-07-21 17:24:08 +0300

    Модуль 2 Функции

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,34 @@
 /**
-удалить все пробелы
-перевернуть строку
-сравнить с первоначальным вариантом
-если равны палиндром, если нет - нет */
-
-/**
  * Функция проверки строки на палиндром
  * @param {string} string - изначальная строка для проверки
  * @return {boolean} - true палиндром, false - не палиндром
  */
-const checkPalindrome = (string) => {
-  string.split("").reverse().join("");
-};
-const x = 5;
+const checkPalindrome = (string) =>
+  string.toLowerCase().replace(/\s/g, '') === string.toLowerCase().replace(/\s/g, '').split("").reverse().join("");
+
+/**
+ * Функция извлечения целого числа из цифр в строке
+ * @param {string} string - изначальная строка для извлечения
+ * @return {number} - целое число
+ */
+const extractNumber = (string) =>
+  +string.toString().match(/\d+/g).join("");
+
+/**
+ * Функция формирования адресов файлов
+ * @param {string} string - исходная строка
+ * @param {number} lengthstr - длинна строки
+ * @param {string} symbol - исходная строка
+ * @return {string} - адрес файла
+ */
+const creatingAddres = (string, lengthstr, symbol) =>
+  string.length > lengthstr ? string : string.padStart(lengthstr, symbol);
+
+/**
+ * Функция проверки длинны строки
+ * @param {string} string - изначальная строка для проверки
+ * @param {number} lengthstr - длинна строки
+ * @return {boolean} - true строка меньше или равно указанной длинне, false - строка длиннее
+ */
+const checkLengthStr = (string, lengthstr) =>
+  string.length <= lengthstr;
