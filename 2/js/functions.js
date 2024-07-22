1a283073882b459083edab44ce6cba3fafdae3b1
Keks <keks@htmlacademy.ru>
2024-07-22 22:05:32 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,53 @@
+/**
+ * Функция проверки строки на палиндром
+ * @param {string} string - изначальная строка для проверки
+ * @return {boolean} - true палиндром, false - не палиндром
+ */
+const checkPalindrome = (string) =>
+  string.toLowerCase().replace(/\s/g, '') === string.toLowerCase().replace(/\s/g, '').split("").reverse().join("");
+
+/**
+ * Функция извлечения целого числа из цифр в строке
+ * @param {string} string - изначальная  строка для извлечения
+ * @return {number} - целое число
+ */
+const extractNumber = (string) =>
+  +string.toString().match(/\d+/g).join("");
+
+/**
+ * Функция формирования адресов файлов
+ * @param {string} string - исходная строка
+ * @param {number} lengthstr - длинна строки
+ * @param {string} symbol - исходная строка
+ * @return {string} - адрес файла
+ */
+const creatingAddres = (string, lengthstr, symbol) =>
+  string.length > lengthstr ? string : string.padStart(lengthstr, symbol);
+
+/**
+ * Функция формирования адресов файлов
+ * @param {string} string - исходная строка
+ * @param {number} lengthstr - длинна строки
+ * @param {string} symbol - исходная строка
+ * @return {string} - адрес файла
+ */
+const creatingAddres = (string, lengthstr, symbol) => {
+  if (string.length > lengthstr) {
+    return string;
+  }
+  const stringCount = lengthstr - string.length;
+  for (let i = 0; i < stringCount; i++) {
+    string = symbol + string;
+  }
+  return string;
+}
+
+/**
+ * Функция проверки длинны строки
+ * @param {string} string - изначальная строка для проверки
+ * @param {number} lengthstr - длинна строки
+ * @return {boolean} - true строка меньше или равно указанной длинне, false - строка длиннее
+ */
+const checkLengthStr = (string, lengthstr) =>
+  string.length <= lengthstr;
+
