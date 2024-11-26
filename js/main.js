be08500792c38a194870499738465bedf6bfd666
Андрей Урамаев <aruramaev@gmail.com>
2024-11-26 21:54:52 +0300

    исправил+

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ renderPictures(photoDescriptions);
 document.querySelectorAll('.picture').forEach((pictureElement) => {
   pictureElement.addEventListener('click', () => {
     const photoId = pictureElement.dataset.id;
-    const photo = photoDescriptions.find((photo) => photo.id === +photoId);
+    const photo = photoDescriptions.find((photo) => photo.id === parseInt(photoId));
     renderBigPicture(photo);
   });
 });
